5d8a3c4cbf747c07d0e136faa4825edb99200e82
Rohan Garg <rgbored2002@gmail.com>
2023-07-22 12:01:58 +0530

    implemented fetching updated list of trains every 5 seconds

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const port = 8080;
 app.use(express.json())
 
 var token = "";
+var trains = {};
 const bodyParameters = {
     "companyName": "Trains Checker", 
     "clientID": "4b1ec91b-de61-458f-803d-384bce4df65d",
@@ -14,24 +15,48 @@ const bodyParameters = {
     "rollNo": "2000320130134",
     "clientSecret": "buyVTPEZjtFPuvTZ",
  };
- 
+ let interval = 1000;
  axios.post( 
    'http://20.244.56.144/train/auth',
    bodyParameters
- ).then((res)=>{token = res.data.access_token;console.log(token)}).catch(console.log);
+ ).then((res)=>{
+     token = res.data.access_token;
+     setInterval(()=>{
+
+         const config = {
+             headers: { Authorization: `Bearer ${token}` }
+         };
+         axios.get('http://20.244.56.144/train/trains', config)
+    .then(res => trains = res.data)
+    .catch(err => console.log(err))
+     }, 5000);
+    
+     console.log(res.data.expires_in);
+     interval = res.data.expires_in;
+     setInterval(() => {
+         axios.post( 
+             'http://20.244.56.144/train/auth',
+             bodyParameters
+             ).then((res)=>{
+                 token = res.data.access_token;
+
+
+             console.log(token);
+             interval = res.data.expires_in;
+         }).catch(console.log);
+      }, interval);
+ }).then(console.log(interval)).catch(console.log);
+//  setInterval(()=>{console.log(interval, token)}, 5000);
+//  console.log(interval);
+
 
 // console.log(token);
 
 // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTAwMDM4NjQsImNvbXBhbnlOYW1lIjoiVHJhaW5zIENoZWNrZXIiLCJjbGllbnRJRCI6IjRiMWVjOTFiLWRlNjEtNDU4Zi04MDNkLTM4NGJjZTRkZjY1ZCIsIm93bmVyTmFtZSI6IiIsIm93bmVyRW1haWwiOiIiLCJyb2xsTm8iOiIyMDAwMzIwMTMwMTM0In0.P19On02UCa_WfQoZ2Kn3IWAR9rlWZfOTezNHKbpFpII";
-const config = {
-    headers: { Authorization: `Bearer ${token}` }
-};
-axios.get('http://20.244.56.144/train/trains', config)
-  .then(res => console.log(res.data))
-  .catch(err => console.log(err))
-
 
   
+
+
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
 })
